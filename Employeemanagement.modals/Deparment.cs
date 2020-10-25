51177f509d63e7c4b021d84527feef073d1313b9
Prashan Raj Kc <64183754+prkprime1@users.noreply.github.com>
2020-10-25 16:00:48 +0545

    Add project files.

---
+++ Employeemanagement.modals/Deparment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Employeemanagement.modals
+{
+    public class Deparment
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
